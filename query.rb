f3be99adb32a065e5fb33a6870c67833c539249f
vivek0293 <vivek0293@outlook.com>
2015-11-09 15:00:17 -0600

    make it better

--- query.rb
+++ query.rb
@@ -5,9 +5,16 @@ begin
 
     con = PG.connect :host => 'ec2-184-73-165-195.compute-1.amazonaws.com', :dbname => 'd62e809t1n9frg', :user => 'kyokfgvbcubmle', 
         :password => 'li_5MHueue-Z_m85bRmt_EOj3E'
-    v = con.exec('SELECT AGE from health')
+    v = con.exec("SELECT AGE,SEX,RACE, LENGTH_OF_STAY, STAY_INDICATOR, TOTAL_CHARGES from health where ID = '1'")
+    puts "QUERY 1"
     v.each do |row|
-        puts "%s" % [ row['AGE'] ]
+        puts "%s %s %s %s %s %s" % [ row['age'], row['sex'], row['race'], row['length_of_stay'], row['stay_indicator'], row['total_charges'] ]
+    end
+
+    w = con.exec("SELECT AGE,SEX,RACE, LENGTH_OF_STAY, STAY_INDICATOR, TOTAL_CHARGES from health where AGE = '3'")
+    puts "QUERY 2"
+    w.each do |row|
+        puts "%s %s %s %s %s %s" % [ row['age'], row['sex'], row['race'], row['length_of_stay'], row['stay_indicator'], row['total_charges'] ]
     end
     puts "DONE"
 rescue PG::Error => e
