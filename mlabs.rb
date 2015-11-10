868ccd6742629761900674bfc7e954f50cc32c86
vivek0293 <vivek0293@outlook.com>
2015-11-10 13:42:41 -0600

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -1,8 +1,10 @@
-require 'rubygems'  
 require 'mongo'
 
 begin
-	db = Mongo::Connection.new("mongo ds031912.mongolab.com:31912").db("heroku_mhz4f7bj")
+	Mongo::Client.new([ 'mongo ds031912.mongolab.com:31912' ], :database => 'heroku_mhz4f7bj')
+	Mongo::Client.new([ 'mongo ds031912.mongolab.com:31912' ], :database => 'heroku_mhz4f7bj', :connect => :direct)
+	Mongo::Client.new('mongodb://heroku_mhz4f7bj/heroku_mhz4f7bj')
+	#db = Mongo::Connection.new("mongo ds031912.mongolab.com:31912").db("heroku_mhz4f7bj")
 
 	puts "Connected"
 
