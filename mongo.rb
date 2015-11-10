6759ef3ef6f450d5eaa59b73f619f7da0dde27e0
vivek0293 <vivek0293@outlook.com>
2015-11-10 13:31:29 -0600

    make it better

--- mongo.rb
+++ mongo.rb
@@ -1,6 +1,6 @@
 require 'rubygems'  
 require 'mongo'
 
-db = Mongo::Connection.new("localhost").db("mydb")
+db = Mongo::Connection.new("mongo ds031912.mongolab.com:31912").db("heroku_mhz4f7bj")
 
 puts "Connected"
