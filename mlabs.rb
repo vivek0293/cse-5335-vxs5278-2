da5e68833211b73fadf3ed111fb3b69d4a693d1d
vivek0293 <vivek0293@outlook.com>
2015-11-11 12:53:53 -0600

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'mongo'
-
+require 'csv'
+require 'JSON'
 begin
 	#client = Mongo::Client.new([ 'localhost:27017' ], :database => 'music')
 	#mongo_client = Mongo::Client.new(['localhost'], 27017)
@@ -12,6 +13,8 @@ begin
 	#puts "Collection created"
 	#mongoimport --db heroku_mhz4f7bj --collection sample --type csv --headerline --file 6339_Dataset_1.csv
 	#mongoimport --db heroku_mhz4f7bj --type csv --headerline --file 6339_Dataset_1.csvmongoimport --host=127.0.0.1 -d database_name -c collection_name --type csv --file csv_location --headerline
-	mongoimport --host=ds031912.mongolab.com:31912 -heroku_mhz4f7bj --type csv --file 6339_Dataset_1.csv --headerline
+	#mongoimport --host=ds031912.mongolab.com:31912 -d heroku_mhz4f7bj --type csv --file 6339_Dataset_1.csv --headerline
+
+	CSV.parse(6339_Dataset_1.csv).to_json
 	puts "file imported"
 end
