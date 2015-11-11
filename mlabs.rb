d9a0b138ef32bc8b3d7585344b8dcf5eaf54b53c
vivek0293 <vivek0293@outlook.com>
2015-11-11 00:25:26 -0600

    make it better

--- mlabs.rb
+++ mlabs.rb
@@ -4,8 +4,8 @@ require 'mongo'
 begin
 	#client = Mongo::Client.new([ 'localhost:27017' ], :database => 'music')
 	#mongo_client = Mongo::Client.new(['localhost'], 27017)
-	db = Mongo::Client.new(["mongo ds031912.mongolab.com:31912"]).db("heroku_mhz4f7bj")
-
+	#db = Mongo::Client.new(["mongo ds031912.mongolab.com:31912"]).db("heroku_mhz4f7bj")
+	client = Mongo::Client.new([ 'ds031912.mongolab.com:31912' ], :database => 'heroku_mhz4f7bj', :user => 'heroku_mhz4f7bj')
 	puts "Connected"
 
 end
